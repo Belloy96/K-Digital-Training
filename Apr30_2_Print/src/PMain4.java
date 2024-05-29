3bc001b9636855aebbf053d4a9d927717e94a3c7
Belloy96 <balmung96@skkukdp.re.kr>
2024-05-29 14:34:08 +0900

    Apr30_2_Print!

---
+++ Apr30_2_Print/src/PMain4.java
@@ -0,0 +1,18 @@
+
+public class PMain4 {
+	public static void main(String[] args) {
+		
+		// 예제 2
+		// 명함 만들기
+		// 이름 / 나이 / 성별 / 키 / 사는 지역 / 별명 (printf)
+		// .bat파일로 실행, 10초뒤 자동종료
+		
+		// Thread.sleep(밀리초) 이후 문법 추가
+		// export로 .jar파일을 만들고
+		// 메모장으로 명령어 java -jar 파일명.jar
+		// 다른이름으로 저장 후 모든 파일로 변경 후 이름은 파일명.bat으로 저장
+		
+		
+		
+	}
+}
